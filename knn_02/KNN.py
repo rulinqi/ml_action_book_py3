61649461df7bd905abfdbe63e443c9b6226bfd0b
rulinqi <1185465226@qq.com>
2017-10-28 18:37:25 +0800

    github学习修改

--- knn_02/KNN.py
+++ knn_02/KNN.py
@@ -7,6 +7,8 @@
 
 import numpy as np
 import operator
+import os
+import matplotlib.pyplot as plt
 
 def createDataSet():
     group = np.array([[1,1.1],[1,1],[0,0],[0,0.1]])
@@ -51,12 +53,68 @@ def classify0(inX, dataSet, labels, k):
     sortedClassCount = sorted(classCount.items(),key = operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
+def file2matrix(filename):
+    """
+    将文本记录转换为Numpy的解析程序
+    :return:
+    """
+    # 1. 得到文件行数
+    fr = open(filename)
+    arrayOLines = fr.readlines()
+    numberOfLines = len(arrayOLines)
+    # 2.创建返回的Numpy矩阵
+    returnMat = np.zeros((numberOfLines,3))
+    classLabelVector = []
+    index = 0
+    # 3.解析文件数据到列表
+    for line in arrayOLines:
+        # 把回车符号给去掉
+        line = line.strip()
+        # 把每一行数据用\t分割
+        listFromLine = line.split('\t')
+        # 把分割好的数据放至数据集，其中index是该样本数据的下标，就是放到第几行
+        returnMat[index, :] = listFromLine[0:3]
+        # 把该样本对应的标签放至标签集，顺序与样本集对应。
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+dataset_path = './dataset'
+filepath = 'datingTestSet2.txt'
+
+
 def run_main():
+    # 1. 测试knn
+    #test_knn()
+
+    # 2. exp 改进约会网站的配对效果
+    exp_dating_knn()
+
+
+def test_knn():
+    #测试knn
     group, labels = createDataSet()
     print(group)
     print(labels)
     k = classify0([0,0], group, labels, 3)
     print(k)
+
+def exp_dating_knn():
+    """
+    knn 改进约会网站的配对效果
+    :return:
+    """
+    # 1. 载入数据
+    datingDataMat, datingLabels = file2matrix(os.path.join(dataset_path, filepath))
+    print(datingLabels[0:20])
+
+    # 2. 使用Matplotlib创建散点图
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # ax.scatter(datingDataMat[:,1], datingDataMat[:,2])
+    ax.scatter(datingDataMat[:,1], datingDataMat[:,2],15 * np.array(datingLabels),15 * np.array(datingLabels))
+    plt.show()
+
+
 
 if __name__ == '__main__':
     run_main()
